47163b57e57393a5a736b2bf7ec6361f97e6ebdc
Parth Soni <parthsoni2004@gmail.com>
2023-01-07 00:05:17 -0400

    initial commit

--- src/GameDemo.java
+++ src/GameDemo.java
@@ -18,7 +18,8 @@ public class GameDemo {
                 if (Board.board[pos.x][pos.y] != null) {
                     System.out.println("Error: there is already a piece on this position");
                     System.out.println("Enter a command (type help for details): ");
-                } else {
+                }
+                else {
                     System.out.println("Enter a name for the new piece");
                     String name = in.next();
                     System.out.println("Enter the colour for the new piece ");
@@ -28,34 +29,31 @@ public class GameDemo {
                         Piece piece = new SlowPiece(name, colour, pos_1);
                         gameBoard.add(piece);
                     }
-
                     if (split.length == 4) {
                         if (split[3].equals("fast")) {
                             Piece piece = new FastPiece(name, colour, pos_1);
                             gameBoard.add(piece);
-                        } else if (split[3].equals("flexible")) {
+                        }
+                        else if (split[3].equals("flexible")) {
                             Piece piece = new SlowFlexible(name, colour, pos_1);
                             gameBoard.add(piece);
                         }
-                    } else {
+                    }
+                    else{
                         Piece piece = new FastFlexible(name, colour, pos_1);
                         gameBoard.add(piece);
                     }
-
                     System.out.println("Enter a command (type help for details): ");
                 }
             }
-
             if (split[0].equals("move")) {
                 pos = new Point(Integer.parseInt(split[1]), Integer.parseInt(split[2]));
                 if (split.length == 4) {
                     gameBoard.move(pos, split[3], 1);
                 }
-
                 if (split.length == 5) {
                     gameBoard.move(pos, split[3], Integer.parseInt(split[4]));
                 }
-
                 System.out.println("Enter a command (type help for details): ");
             }
 
@@ -68,13 +66,11 @@ public class GameDemo {
                 System.out.println("exit: Exits the program.");
                 System.out.println("Enter a command (type help for details): ");
             }
-
             if (split[0].equals("print")) {
                 gameBoard.display();
                 System.out.println("Enter a command (type help for details): ");
             }
         }
-
         System.out.println("Done");
     }
 }
